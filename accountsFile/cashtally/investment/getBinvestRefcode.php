0f2eb6c583c637ac650519b614cb95c5ce714c57
Rabi-Jerome <rabijerome.p@gmail.com>
2025-05-26 18:51:46 +0530

    correction on cash tally investment code

--- accountsFile/cashtally/investment/getBinvestRefcode.php
+++ accountsFile/cashtally/investment/getBinvestRefcode.php
@@ -43,7 +43,7 @@ if ($codeAvailable->rowCount() > 0) {
 }
 ///////////////////////////////////////////////////////////////////////////////////////////
 
-$ref_code = str_replace(`"`,'',$ref_code);
+$ref_code = str_replace('"','',$ref_code);
 
 echo $ref_code;
 
