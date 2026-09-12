2279590a12a1cf96b78e463ada220f2b78614d4a
Pugal <rnaveen0011@gmail.com>
2026-09-12 16:45:46 +0530

    remove famil info mobile number manditory

--- js/update.js
+++ js/update.js
@@ -210,7 +210,7 @@ $(document).ready(function () {
             authorize : $("#authorize").val()
         }
 
-        if (famData.famname != "" && famData.relationship != "" && famData.relation_Mobile != ""  && famData.relation_live_deceased != "") {
+        if (famData.famname != "" && famData.relationship != ""  && famData.relation_live_deceased != "") {
             $.ajax({
                 url: 'updateFile/update_family_submit.php',
                 type: 'POST',
@@ -274,11 +274,11 @@ $(document).ready(function () {
             //     $('#famaadharCheck').hide();
             // }
 
-            if (famData.relation_Mobile == "" || famData.relation_Mobile.length < 10) {
-                $('#fammobileCheck').show();
-            } else {
-                $('#fammobileCheck').hide();
-            }
+            // if (famData.relation_Mobile == "" || famData.relation_Mobile.length < 10) {
+            //     $('#fammobileCheck').show();
+            // } else {
+            //     $('#fammobileCheck').hide();
+            // }
             
             // if (famData.relation_dob == "") {
             //     $("#famdobCheck").show();
